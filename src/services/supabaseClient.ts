a4b3198d77a8cee0beb75d892ac296d74026d23d
Dimitar-Totov <dimitar.totov@abv.bg>
2026-07-17 19:57:00 +0300

    Add configuration to supabase to store auth data in session storage

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -10,4 +10,9 @@ if (!supabaseUrl || !supabaseAnonKey) {
   );
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: window.sessionStorage,
+    persistSession: true,
+  },
+});
